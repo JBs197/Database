6469c062ec9b5730694b1f7d62473036a342ffe3
JBs197 <75170573+JBs197@users.noreply.github.com>
2021-01-21 07:00:13 -0800

    Add files via upload

--- Database.cpp
+++ Database.cpp
@@ -19,7 +19,6 @@ mutex mcout8, mcout16, mpo, mcallback, merror;
 vector<thread::id> thread_list;
 vector<vector<wstring>> objects;
 ofstream ERR(error_log_path, ios_base::out | ios_base::app);
-bool debug = 0;
 
 typedef struct {
 	HWND       hWindow;
@@ -133,13 +132,14 @@ int clean(wstring& out, int mode)
 		pos2 = out.find(L']', pos1);
 		out.erase(pos1, pos2 - pos1 + 1);
 	}
+	/*
 	pos1 = out.find(L'(', 0);
 	if (pos1 < out.size())
 	{
 		pos2 = out.find(L')', pos1);
 		out.erase(pos1, pos2 - pos1 + 1);
 	}
-
+	*/
 	if (mode == 1)
 	{
 		pos1 = out.find(L'\'', 0);
@@ -308,6 +308,100 @@ vector<vector<wstring>> get_subfolders2(wstring root_folder)
 	return subfolders2;
 }
 
+// Query the user, and return the full path of the chosen catalogue folder.
+wstring list_maker(vector<wstring> list, int number_of_columns, int spaces_per_entry)
+{
+	int size;
+	int width;
+	int count = 0;
+	wstring output;
+	for (int ii = 0; ii < list.size(); ii++)
+	{
+		if (count < number_of_columns)
+		{
+			output.append(to_wstring(ii + 1));
+			output.append(L")  ");
+			output.append(list[ii]);
+			size = list[ii].size();
+			for (int jj = 0; jj < (spaces_per_entry - size); jj++)
+			{
+				output.push_back(L' ');
+			}
+			count++;
+		}
+		else
+		{
+			output.append(L"\r\n");
+			output.append(to_wstring(ii + 1));
+			output.append(L")  ");
+			output.append(list[ii]);
+			size = list[ii].size();
+			for (int jj = 0; jj < (spaces_per_entry - size); jj++)
+			{
+				output.push_back(L' ');
+			}
+			count = 1;
+		}
+	}
+	return output;
+}
+wstring query_catalogue(wstring root_folder)
+{
+	vector<wstring> year_folders = get_subfolders(root_folder);
+	vector<wstring> years;
+	size_t pos1;
+	wstring list, chosen_number, chosen_year, temp1;
+	int NUMBER;
+
+	for (int ii = 0; ii < year_folders.size(); ii++)
+	{
+		pos1 = year_folders[ii].rfind(L'\\');
+		temp1 = year_folders[ii].substr(pos1 + 1);
+		years.push_back(temp1);
+	}
+	list = list_maker(years, 3, 20);
+	wcout << L"From the listed years, choose a number." << endl << endl;
+	wcout << list << endl << endl;
+	wcin >> chosen_number;
+	try
+	{
+		NUMBER = stoi(chosen_number);
+	}
+	catch (invalid_argument& ia)
+	{
+		wcout << L"Invalid value for the year. Press any key, then try again.";
+		system("pause");
+		exit(EXIT_FAILURE);
+	}
+	chosen_year = year_folders[NUMBER - 1];
+
+	vector<wstring> cata_folders = get_subfolders(chosen_year);
+	vector<wstring> catalogues;
+	wstring chosen_catalogue;
+	for (int ii = 0; ii < cata_folders.size(); ii++)
+	{
+		pos1 = cata_folders[ii].rfind(L'\\');
+		temp1 = cata_folders[ii].substr(pos1 + 1);
+		catalogues.push_back(temp1);
+	}
+	list = list_maker(catalogues, 3, 20);
+	wcout << L"From the listed catalogues, choose a number." << endl << endl;
+	wcout << list << endl << endl;
+	wcin >> chosen_number;
+	try
+	{
+		NUMBER = stoi(chosen_number);
+	}
+	catch (invalid_argument& ia)
+	{
+		wcout << L"Invalid value for the catalogue. Press any key, then try again.";
+		system("pause");
+		exit(EXIT_FAILURE);
+	}
+	chosen_catalogue = cata_folders[NUMBER - 1];
+	return chosen_catalogue;
+}
+
 int callback(void* NotUsed, int argc, char** argv, char** column_name)
 {
 	for (int ii = 0; ii < argc; ii++)
@@ -434,9 +528,10 @@ int download(wstring url, wstring folder, wstring filename)
 	HINTERNET hrequest = NULL;
 	DWORD bytes_available;
 	DWORD bytes_read = 0;
-	LPSTR bufferA = new CHAR[1];
+	unsigned char* ubufferA = new unsigned char[1];
 	int size1, size2;
-	string fileA;
+	wstring fileW;
+	bool special_char = 0;
 
 	hint = InternetOpenW(agent.c_str(), INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
 	if (hint)
@@ -461,25 +556,36 @@ int download(wstring url, wstring folder, wstring filename)
 		{
 			bytes_available = 0;
 			InternetQueryDataAvailable(hrequest, &bytes_available, 0, 0);
-			bufferA = new CHAR[bytes_available];
-			if (!InternetReadFile(hrequest, (LPVOID)bufferA, bytes_available, &bytes_read))
+			ubufferA = new unsigned char[bytes_available];
+			if (!InternetReadFile(hrequest, ubufferA, bytes_available, &bytes_read))
 			{
 				warn(L"InternetReadFile");
 				return 4;
 			}
-			fileA.append(bufferA, bytes_available);
+			for (int ii = 0; ii < bytes_available; ii++)
+			{
+				fileW.push_back((wchar_t)ubufferA[ii]);
+				if (fileW.back() == L'\x00C3' && !special_char)
+				{
+					special_char = 1;
+				}
+				else if (special_char)
+				{
+					fileW[fileW.size() - 2] = fileW[fileW.size() - 1] + 64;
+					fileW.pop_back();
+					special_char = 0;
+				}
+			}
 		} while (bytes_available > 0);
-		delete[] bufferA;
+		delete[] ubufferA;
 	}
 	else { warn(L"HttpSendRequest"); return 5; }
 
-	wstring fileW = utf8to16(fileA);
-	HANDLE hprinter = CreateFileW(filepath.c_str(), (GENERIC_READ | GENERIC_WRITE), (FILE_SHARE_READ | FILE_SHARE_WRITE), NULL, CREATE_NEW, 0, NULL);
+	HANDLE hprinter = CreateFileW(filepath.c_str(), (GENERIC_READ | GENERIC_WRITE), (FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE), NULL, CREATE_ALWAYS, 0, NULL);
 	if (hprinter == INVALID_HANDLE_VALUE) { warn(L"CreateFile"); return 6; }
 	DWORD bytes_written;
 	DWORD file_size = fileW.size() * 2;
 	if (!WriteFile(hprinter, fileW.c_str(), file_size, &bytes_written, NULL)) { warn(L"WriteFile"); return 7; }
-	if (!CloseHandle(hprinter)) { warn(L"CloseHandle-download"); }
 
 	if (hrequest) { InternetCloseHandle(hrequest); }
 	if (hconnect) { InternetCloseHandle(hconnect); }
@@ -516,6 +622,7 @@ int CSV_DB::read()
 	vector<wstring> sub_columns;
 	vector<wstring> temp_columns;
 	vector<wstring> temp_headings = { L"\0" };
+	wchar_t math;
 	int spaces = 0;
 	int indentation = 0;
 
@@ -526,6 +633,11 @@ int CSV_DB::read()
 	{
 		pos1 = wfile.find(L'=', pos1 + 1);
 		if (pos1 > wfile.size()) { break; }
+		math = wfile[pos1 - 1];
+		if (math == L'<' || math == L'>')
+		{
+			continue;
+		}
 		variables.push_back(vector<string>());
 
 		pos2 = wfile.rfind(L'"', pos1); pos2++;
@@ -575,7 +687,10 @@ int CSV_DB::read()
 			temp_columns.clear();
 			continue;
 		}
-		else if (temp_columns[0] == L"Note") { break; }
+		else if (temp_columns[0] == L"Note") 
+		{
+			break;
+		}
 		else
 		{
 			if (spaces == indentation) 
@@ -1195,7 +1310,14 @@ vector<vector<CATA_DB>> initialize(sqlite3*& database, wstring root_folder)
 			mtcout16(temp1);
 		}
 	}
+	return cata_tree;
+}
 
+// Query the user for a single catalogue, then build a table and extract data from it.
+vector<vector<CATA_DB>> init_debug(sqlite3*& database, wstring cata_folder)
+{
+	vector<vector<CATA_DB>> cata_tree(1, vector<CATA_DB>());
+	cata_tree[0].push_back(tablemaker(database, cata_folder));
 	return cata_tree;
 }
 
@@ -1203,16 +1325,18 @@ int main()
 {
 	setlocale(LC_ALL, ".UTF8");
 	sqlite3* database = createDB(local_directory);
-	vector<vector<CATA_DB>> catalogue_tree = initialize(database, local_directory);
-	/*
-	vector<wstring> subfolders; 
-	sqlite3* database = createDB(year, subfolders);
-	for (int ii = 0; ii < subfolders.size(); ii++) 
+	vector<vector<CATA_DB>> catalogue_tree;
+	wstring catalogue_path;
+	if (debug)
 	{
-		tablemaker(database, subfolders[ii]);
+		catalogue_path = query_catalogue(local_directory);
+		catalogue_tree = init_debug(database, catalogue_path);
+	}
+	else
+	{
+		catalogue_tree = initialize(database, local_directory);
 	}
 	system("pause");
-	*/
 	return 0;
 }
 
